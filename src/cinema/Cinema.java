81faee4675876aaa713fa414ced1b37cc387c066
Anton Kuznetsov <antkuznetsov@yandex.ru>
2021-07-22 16:17:01 +0200

    Implemented Stage 1

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -1,7 +1,30 @@
 package cinema;
 
 public class Cinema {
+    private final String [][] hall;
+
     public static void main(String[] args) {
-        // Write your code here
+        Cinema app = new Cinema(7, 8);
+        app.printHallPlan();
+    }
+
+    public Cinema(int rows, int seats) {
+        hall = new String[rows][seats];
+    }
+
+    private void printHallPlan() {
+        System.out.println("Cinema:");
+        System.out.print("  ");
+        for (int i = 0; i < hall[0].length; i++) {
+            System.out.printf("%s ", i + 1);
+        }
+        System.out.println();
+        for (int i = 0; i < hall.length; i++) {
+            System.out.printf("%d ", i + 1);
+            for (int j = 0; j < hall[i].length; j++) {
+                System.out.print("S ");
+            }
+            System.out.println();
+        }
     }
 }
